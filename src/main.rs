31f0d6b26c6bbdafcd8f55a4aa9195eee60d8b85
Matt Keeter <matt.j.keeter@gmail.com>
2024-01-06 18:44:13 -0500

    Solved teleporter

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,187 @@
-use std::{collections::HashSet, io::BufRead};
+use std::{
+    collections::{BTreeMap, HashMap, HashSet},
+    io::BufRead,
+};
 
 const CHALLENGE: &[u8] = include_bytes!("../challenge.bin");
 const MASK: u16 = 32767;
 
 #[derive(Clone, Eq, PartialEq, Hash)]
 struct Vm {
-    memory: Vec<u16>,
+    memory: Memory,
     register: [u16; 8],
     stack: Vec<u16>,
     ip: usize,
+}
+
+#[derive(Clone, Eq, PartialEq, Hash)]
+struct Memory(Vec<u16>);
+
+impl std::ops::Index<u16> for Memory {
+    type Output = u16;
+    fn index(&self, i: u16) -> &Self::Output {
+        &self.0[i as usize]
+    }
+}
+
+impl std::ops::IndexMut<u16> for Memory {
+    fn index_mut(&mut self, i: u16) -> &mut Self::Output {
+        let i = i as usize;
+        if i >= self.0.len() {
+            self.0.resize(i + 1, 0);
+        }
+        &mut self.0[i]
+    }
+}
+
+impl Memory {
+    /// Reads the value from the instruction pointer, incrementing it
+    fn val(&self, ip: &mut usize) -> Value {
+        let v = self.0[*ip].into();
+        *ip += 1;
+        v
+    }
+
+    /// Reads a register index at the instruction pointer, incrementing it
+    fn reg(&self, ip: &mut usize) -> Register {
+        let v = self.val(ip);
+        let Value::Register(reg) = v else {
+            panic!("not a register: {v:?}");
+        };
+        reg
+    }
+    /// Reads the next opcode from the machine, incrementing `ip`
+    fn read(&self, ip: &mut usize) -> Op {
+        let op = self.0[*ip];
+        *ip += 1;
+        match op {
+            0 => Op::Halt,
+            1 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                Op::Set(a, b)
+            }
+            2 => {
+                let a = self.val(ip);
+                Op::Push(a)
+            }
+            3 => {
+                let a = self.reg(ip);
+                Op::Pop(a)
+            }
+            4 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                let c = self.val(ip);
+                Op::Eq(a, b, c)
+            }
+            5 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                let c = self.val(ip);
+                Op::Gt(a, b, c)
+            }
+            6 => {
+                let a = self.val(ip);
+                Op::Jmp(a)
+            }
+            7 => {
+                let a = self.val(ip);
+                let b = self.val(ip);
+                Op::Jt(a, b)
+            }
+            8 => {
+                let a = self.val(ip);
+                let b = self.val(ip);
+                Op::Jf(a, b)
+            }
+            9 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                let c = self.val(ip);
+                Op::Add(a, b, c)
+            }
+            10 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                let c = self.val(ip);
+                Op::Mult(a, b, c)
+            }
+            11 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                let c = self.val(ip);
+                Op::Mod(a, b, c)
+            }
+            12 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                let c = self.val(ip);
+                Op::And(a, b, c)
+            }
+            13 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                let c = self.val(ip);
+                Op::Or(a, b, c)
+            }
+            14 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                Op::Not(a, b)
+            }
+            15 => {
+                let a = self.reg(ip);
+                let b = self.val(ip);
+                Op::Rmem(a, b)
+            }
+            16 => {
+                let a = self.val(ip);
+                let b = self.val(ip);
+                Op::Wmem(a, b)
+            }
+            17 => {
+                let a = self.val(ip);
+                Op::Call(a)
+            }
+            18 => Op::Ret,
+            19 => {
+                let a = self.val(ip);
+                Op::Out(a)
+            }
+            20 => {
+                let a = self.reg(ip);
+                Op::In(a)
+            }
+            21 => Op::Noop,
+            i => panic!("unimplemented instruction {i}"),
+        }
+    }
+
+    /// Disassemble a function beginning at the given instruction
+    #[allow(unused)]
+    fn disassemble(&self, ip: usize) {
+        let mut seen = BTreeMap::new();
+        let mut todo = vec![ip];
+        while let Some(mut ip) = todo.pop() {
+            let op = self.read(&mut ip);
+            if seen.insert(ip, op).is_none() {
+                if !matches!(op, Op::Ret) {
+                    todo.push(ip);
+                }
+                match op {
+                    Op::Jmp(Value::Literal(a)) => todo.push(a as usize),
+                    Op::Jt(_, Value::Literal(a)) => todo.push(a as usize),
+                    Op::Jf(_, Value::Literal(a)) => todo.push(a as usize),
+                    Op::Jmp(..) | Op::Jt(..) | Op::Jf(..) => print!("OH NO"),
+                    _ => (),
+                }
+            }
+        }
+        for (ip, op) in seen {
+            println!("{ip}: {op}");
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -35,6 +208,35 @@ enum Op {
     Out(Value),
     In(Register),
     Noop,
+}
+
+impl std::fmt::Display for Op {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Op::Halt => write!(f, "halt"),
+            Op::Set(a, b) => write!(f, "{a} = {b}"),
+            Op::Push(a) => write!(f, "push {a}"),
+            Op::Pop(a) => write!(f, "pop  {a}"),
+            Op::Eq(a, b, c) => write!(f, "{a}  = {b} == {c}"),
+            Op::Gt(a, b, c) => write!(f, "{a} = {b} > {c}"),
+            Op::Jmp(a) => write!(f, "jmp  {a}"),
+            Op::Jt(a, b) => write!(f, "jt   {a} {b}"),
+            Op::Jf(a, b) => write!(f, "jf   {a} {b}"),
+            Op::Add(a, b, c) => write!(f, "{a} = {b} + {c}"),
+            Op::Mult(a, b, c) => write!(f, "{a} = {b} * {c}"),
+            Op::Mod(a, b, c) => write!(f, "{a} = {b} % {c}"),
+            Op::And(a, b, c) => write!(f, "{a} = {b} & {c}"),
+            Op::Or(a, b, c) => write!(f, "{a} = {b} | {c}"),
+            Op::Not(a, b) => write!(f, "not  {a} = !{b}"),
+            Op::Rmem(a, b) => write!(f, "rmem {a} {b}"),
+            Op::Wmem(a, b) => write!(f, "wmem {a} {b}"),
+            Op::Call(a) => write!(f, "call {a}"),
+            Op::Ret => write!(f, "ret"),
+            Op::Out(a) => write!(f, "out  {a}"),
+            Op::In(a) => write!(f, "in   {a}"),
+            Op::Noop => write!(f, "noop"),
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -109,7 +311,7 @@ impl Vm {
             .map(|b| u16::from_le_bytes([b[0], b[1]]))
             .collect();
         Self {
-            memory,
+            memory: Memory(memory),
             register: [0; 8],
             stack: vec![],
             ip: 0,
@@ -124,132 +326,9 @@ impl Vm {
         }
     }
 
-    /// Reads the value from the instruction pointer, incrementing it
-    fn val(&mut self) -> Value {
-        let v = self.memory[self.ip].into();
-        self.ip += 1;
-        v
-    }
-
-    /// Reads a register index at the instruction pointer, incrementing it
-    fn reg(&mut self) -> Register {
-        let v = self.val();
-        let Value::Register(reg) = v else {
-            panic!("not a register: {v:?}");
-        };
-        reg
-    }
-
-    /// Reads the next opcode from the machine, incrementing `self.ip`
-    fn next(&mut self) -> Op {
-        let op = self.memory[self.ip];
-        self.ip += 1;
-        match op {
-            0 => Op::Halt,
-            1 => {
-                let a = self.reg();
-                let b = self.val();
-                Op::Set(a, b)
-            }
-            2 => {
-                let a = self.val();
-                Op::Push(a)
-            }
-            3 => {
-                let a = self.reg();
-                Op::Pop(a)
-            }
-            4 => {
-                let a = self.reg();
-                let b = self.val();
-                let c = self.val();
-                Op::Eq(a, b, c)
-            }
-            5 => {
-                let a = self.reg();
-                let b = self.val();
-                let c = self.val();
-                Op::Gt(a, b, c)
-            }
-            6 => {
-                let a = self.val();
-                Op::Jmp(a)
-            }
-            7 => {
-                let a = self.val();
-                let b = self.val();
-                Op::Jt(a, b)
-            }
-            8 => {
-                let a = self.val();
-                let b = self.val();
-                Op::Jf(a, b)
-            }
-            9 => {
-                let a = self.reg();
-                let b = self.val();
-                let c = self.val();
-                Op::Add(a, b, c)
-            }
-            10 => {
-                let a = self.reg();
-                let b = self.val();
-                let c = self.val();
-                Op::Mult(a, b, c)
-            }
-            11 => {
-                let a = self.reg();
-                let b = self.val();
-                let c = self.val();
-                Op::Mod(a, b, c)
-            }
-            12 => {
-                let a = self.reg();
-                let b = self.val();
-                let c = self.val();
-                Op::And(a, b, c)
-            }
-            13 => {
-                let a = self.reg();
-                let b = self.val();
-                let c = self.val();
-                Op::Or(a, b, c)
-            }
-            14 => {
-                let a = self.reg();
-                let b = self.val();
-                Op::Not(a, b)
-            }
-            15 => {
-                let a = self.reg();
-                let b = self.val();
-                Op::Rmem(a, b)
-            }
-            16 => {
-                let a = self.val();
-                let b = self.val();
-                Op::Wmem(a, b)
-            }
-            17 => {
-                let a = self.val();
-                Op::Call(a)
-            }
-            18 => Op::Ret,
-            19 => {
-                let a = self.val();
-                Op::Out(a)
-            }
-            20 => {
-                let a = self.reg();
-                Op::In(a)
-            }
-            21 => Op::Noop,
-            i => panic!("unimplemented instruction {i}"),
-        }
-    }
-
     fn step(&mut self, input: Option<u8>) -> Status {
-        match self.next() {
+        let op = self.memory.read(&mut self.ip);
+        match op {
             Op::Halt => return Status::Halt,
             Op::Set(a, b) => {
                 self[a] = self.get(b);
@@ -299,13 +378,10 @@ impl Vm {
                 self[a] = !self.get(b) & MASK;
             }
             Op::Rmem(a, b) => {
-                self[a] = self.memory[self.get(b) as usize];
+                self[a] = self.memory[self.get(b)];
             }
             Op::Wmem(a, b) => {
-                let a = self.get(a) as usize;
-                if a >= self.memory.len() {
-                    self.memory.resize(a + 1, 0);
-                }
+                let a = self.get(a);
                 self.memory[a] = self.get(b)
             }
             Op::Call(a) => {
@@ -347,13 +423,24 @@ impl Game {
         let mut iter = command.bytes().chain(std::iter::once(b'\n'));
         let mut i = iter.next();
         let mut out = vec![];
+        let mut n = 0;
         let halt = loop {
+            n += 1;
+            if n > 5000000 {
+                panic!("timeout");
+            }
             let r = self.vm.step(i);
+            if self.vm.ip == 6072 {
+                println!("{} {}", self.vm.register[0], self.vm.register[1]);
+            }
             match r {
                 Status::Continue(true) => i = iter.next(),
                 Status::Continue(false) => (),
                 Status::Halt => break true,
-                Status::Out(c) => out.push(c),
+                Status::Out(c) => {
+                    print!("{}", char::from_u32(c as u32).unwrap());
+                    out.push(c);
+                }
                 Status::NeedsInput => break false,
             }
         };
@@ -369,9 +456,8 @@ impl Game {
                 continue;
             }
             print!("{line}");
-            let (desc, halt) = self.step(line);
+            let (_desc, halt) = self.step(line);
             assert!(!halt);
-            print!("{desc}");
         }
     }
 }
@@ -402,13 +488,9 @@ fn find_room_with_item(
     assert!(header.is_empty());
     for e in room.exits {
         let mut game = game.clone();
-        let (desc, halt) = game.step(&e);
+        let (_desc, halt) = game.step(&e);
         if halt {
-            println!("{desc}");
             return None;
-        }
-        if desc.split_whitespace().any(|w| w.len() == 12) {
-            print!("{desc}");
         }
         if let Some(r) = find_room_with_item(game, seen) {
             return Some(r);
@@ -574,18 +656,55 @@ fn main() {
         ",
     );
 
-    for i in 1..u16::MAX {
-        println!("{i}");
-        let mut game = game.clone();
-        game.vm.register[7] = i;
-        let o = game.step("use teleporter");
-        println!("{o:?}");
-    }
-    //game.autoplay("use teleporter");
+    // Found using `find_checksum` below, which is very slow
+    game.vm.register[7] = 25734;
+
+    game.vm.memory[5507] = 6; // Preload r0 with the correct answer
+    game.vm.memory[5511] = 21; // replace calibration call with noop
+    game.vm.memory[5512] = 21; // replace calibration jump with noop
+
+    game.autoplay("use teleporter");
+
+    // We're now on the beach!
+    game.autoplay(
+        "
+            west
+            north
+            east
+            north
+            north
+            north
+            north
+            north
+            north
+            east
+            take journal
+            look journal
+            west
+            north
+            north
+        ",
+    );
+
+    // WELCOME TO THE ORB ZONE
+    //
+    // The number 22 is carved into the orb's pedastal
+    //
+    //    *  8  - 1V
+    //    4  * 11  *
+    //    +  4  - 18
+    //    O  -  9  *
+    //
+    //  The orb starts at weight 22 and we want to hit 30 at the vault (upper
+    //  right corner)
+    game.autoplay(
+        "
+            take orb
+        ",
+    );
 
     for line in std::io::stdin().lock().lines() {
-        let (desc, halt) = game.step(&line.unwrap());
-        print!("{desc}");
+        let (_desc, halt) = game.step(&line.unwrap());
         if halt {
             break;
         }
@@ -602,4 +721,76 @@ fn main() {
         // keep going
     }
     */
+}
+
+/// Rust implementation of the teleportation checksum procedure
+///
+/// The procedure is defined by the function beginning at 6049, taking `r0` and
+/// `r1` as inputs and returning a value in `r0`.
+///
+/// ```
+/// 6049: jt   r0 6057
+/// 6052: r0 = r1 + 1
+/// 6056: ret
+/// 6057: jt   r1 6070
+/// 6060: r0 = r0 + 32767
+/// 6064: r1 = r7
+/// 6067: call 6049
+/// 6069: ret
+/// 6070: push r0
+/// 6072: r1 = r1 + 32767
+/// 6076: call 6049
+/// 6078: r1 = r0
+/// 6081: pop  r0
+/// 6083: r0 = r0 + 32767
+/// 6087: call 6049
+/// 6089: ret
+/// ```
+///
+/// This translates to roughly
+/// ```
+/// def f(r0, r1):
+///     if r0 == 0:
+///         return r1 + 1
+///     elif r1 == 0:
+///         return f(r0 - 1, r7)
+///     else:
+///         r1 = f(r0, r1 - 1)
+///         return f(r0 - 1, r1)
+/// ```
+///
+/// The Rust implementation memoizes the (r0, r1) -> r0
+fn checksum(r0: u16, r1: u16, r7: u16, seen: &mut [u16]) -> u16 {
+    // 3 bits for r0, 15 for r1
+    assert!(r0 <= 4);
+    let key = (r0 as usize) | (r1 as usize) << 3;
+    if seen[key] == u16::MAX {
+        seen[key] = if r0 == 0 {
+            r1.wrapping_add(1) & MASK
+        } else if r1 == 0 {
+            checksum(r0 - 1, r7, r7, seen)
+        } else {
+            let r1 = checksum(r0, r1 - 1, r7, seen);
+            checksum(r0 - 1, r1, r7, seen)
+        };
+    }
+    seen[key]
+}
+
+/// Finds a value for r7 such that f(4, 1) == 6
+///
+/// This is very slow, so I only ran it once to find the magic number.
+#[allow(unused)]
+fn find_checksum() -> u16 {
+    for i in 1..32767 {
+        let mut seen = vec![u16::MAX; 1 << 18];
+        if i % 1000 == 0 {
+            println!("{i}");
+        }
+        let e = checksum(4, 1, i, &mut seen);
+        if e == 6 {
+            return i;
+        }
+    }
+    panic!("no value found");
 }
